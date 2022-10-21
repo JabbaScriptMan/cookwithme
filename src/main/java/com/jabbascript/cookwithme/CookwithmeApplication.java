d0287299b56512d4ef2f5834e5472ca030013388
JabbaScriptMan <maksymilian.zaczek1@gmail.com>
2022-10-21 04:57:53 +0200

    Spring initialized

---
+++ src/main/java/com/jabbascript/cookwithme/CookwithmeApplication.java
@@ -0,0 +1,13 @@
+package com.jabbascript.cookwithme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CookwithmeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CookwithmeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jabbascript/cookwithme/CookwithmeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jabbascript.cookwithme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CookwithmeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
